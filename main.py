00bbf08ff9568f62cd95b41b408c1653b802c9b3
wirarya2005 <wirarya.tjoa@gmail.com>
2025-01-03 08:33:03 +0700

    init dan nambah beberapa fungsi

---
+++ main.py
@@ -0,0 +1,9 @@
+import Tams.matematika as math
+
+print(math.tambah(5, 4))
+print(math.kurang(1, 1, 3))
+print(math.faktorial(0))
+print(math.bagi(5,1))
+print(math.kali(2,3,4))
+print(math.nilai_terkecil([3,555,23,444,5]))
+print(math.nilai_terbesar([3,555,23,444,5]))
